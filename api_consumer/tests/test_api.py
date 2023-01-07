431111ef538ffe008b54c50559d7a80e6901b5d4
Valentin B <valbou@valbou.fr>
2023-01-07 20:36:52 +0100

    Move tests

--- api_consumer/tests/test_api.py
+++ api_consumer/tests/test_api.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
+
 from requests import Response
 
 from api_consumer.api import Api
@@ -8,7 +9,6 @@ from api_consumer.api import Api
 class TestApi(TestCase):
     def test_default_values_api(self):
         api = Api()
-        self.assertEqual(api.token, "")
         self.assertEqual(api.url, "")
         self.assertEqual(api.output, "json")
         self.assertEqual(api.prev, "")
@@ -26,7 +26,6 @@ class TestApi(TestCase):
         api.config(
             url="http://test.com",
         )
-        self.assertEqual(api.token, "")
         self.assertEqual(api.url, "http://test.com")
         self.assertEqual(api.output, "json")
         self.assertEqual(api.prev, "")
---
+++ api_consumer/tests/test_model.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from api_consumer.model import Model
+
+
+class TestModel(TestCase):
+    pass
+
+
+# TODO:
+# - Mock API methods
+# - Test model methods
