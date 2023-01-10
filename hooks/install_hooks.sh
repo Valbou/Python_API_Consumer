8b5ce195612ba21a2c7440e7d5752f70a37e1570
Valentin B <valbou@valbou.fr>
2023-01-10 20:06:24 +0100

    Add issue ref for futur commits

--- hooks/install_hooks.sh
+++ hooks/install_hooks.sh
@@ -2,9 +2,11 @@
 
 cd ../.git/hooks/
 
+ln -s -f ../../hooks/commit-msg.py ./commit-msg
 ln -s -f ../../hooks/pre-commit.py ./pre-commit
 ln -s -f ../../hooks/pre-push.py ./pre-push
 
+chmod +x commit-msg
 chmod +x pre-commit
 chmod +x pre-push
 
